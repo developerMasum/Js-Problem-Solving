6b8035f38b633baa34da644a05e0c5d2fbf273c6
developerMasum <developer.masum2001@gmail.com>
2024-09-15 23:33:26 +0600

    added problem-2

--- problem.js
+++ problem.js
@@ -8,21 +8,45 @@
 // const joinData = reverseData.join("");
 // console.log(joinData);
 
-const reversedData = (text) => {
-  const splitData = text.split("");
-  const reversedData = splitData.reverse();
-  const joinData = reversedData.join("");
-  return joinData;
-};
-console.log(reversedData("hello"));
+// const reversedData = (text) => {
+//   const splitData = text.split("");
+//   const reversedData = splitData.reverse();
+//   const joinData = reversedData.join("");
+//   return joinData;
+// };
+// console.log(reversedData(""));
 
-// alternative solution
-const reversedDataLoop = (text) => {
-  let reversed = "";
-  for (let i = text.length - 1; i >= 0; i--) {
-    reversed += text[i];
-  }
-  return reversed;
+// // alternative solution
+// const reversedDataLoop = (text) => {
+//   let reversed = "";
+//   for (let i = text.length - 1; i >= 0; i--) {
+//     reversed += text[i];
+//   }
+//   return reversed;
+// };
+
+// console.log("solution alternative : ->", reversedDataLoop("hello"));
+
+// problem 2
+// const givenData = "imabookaaaaaaaaaaeeeeeeee";
+// const vowel = "aeiou";
+
+// const splitVowelFromGivenData = givenData.toLowerCase().split("");
+
+// const vowelCount = splitVowelFromGivenData.filter((t) =>
+//   vowel.includes(t)
+// ).length;
+
+// console.log(vowelCount);
+
+const countVowels = (text) => {
+  const vowel = "aeiou";
+  const count = text
+    .toLowerCase()
+    .split("")
+    .filter((singleLetter) => vowel.includes(singleLetter)).length;
+
+  return count;
 };
 
-console.log(reversedDataLoop("hello"));
+console.log(countVowels("aaeennoo"));
